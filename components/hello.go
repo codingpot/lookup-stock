328b758ad0cdbf22f8d09e8efc45a0cfd348616b
Chanyub.Park <mrchypark@gmail.com>
2022-02-01 09:12:35 +0000

    feat: add install button

--- components/hello.go
+++ components/hello.go
@@ -6,8 +6,28 @@ import (
 
 type Hello struct {
 	app.Compo
+
+	//name             string
+	isAppInstallable bool
+}
+
+func (h *Hello) OnMount(ctx app.Context) {
+	h.isAppInstallable = ctx.IsAppInstallable()
+}
+
+func (h *Hello) onInstallButtonClicked(ctx app.Context, e app.Event) {
+	ctx.ShowAppInstallPrompt()
 }
 
 func (h *Hello) Render() app.UI {
-	return app.H1().Text("Hello World!")
+	return app.Div().
+		Body(
+			app.H1().Text("Hello World!"),
+
+			app.If(h.isAppInstallable,
+				app.Button().
+					Text("Install App").
+					OnClick(h.onInstallButtonClicked),
+			),
+		)
 }
